d203cefe737db8dc956e285fde92abaf2470b57c
FabioFattori <fabiofattoriw@gmail.com>
2020-11-04 11:43:40 +0100

    addizione

---
+++ Prova 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prova_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int mamt = 0;
+            mamt += 5;
+
+            Console.WriteLine(mamt);
+
+            Console.ReadKey();
+        }
+    }
+}
